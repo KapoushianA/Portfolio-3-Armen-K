7efbc4aae2ab1400206272906291c1236e8c48cb
KapoushianA <armen.goml@yahoo.com>
2015-01-13 11:29:31 -0800

    hi larrimore done with 4

--- public_html/script.js
+++ public_html/script.js
@@ -9,11 +9,28 @@ $("document").ready(function(){
     
     $("p").css("color", "blue");
     
-    $("h1").bind("mouseover", mouseOverMe).bind("mouseout", mouseOutMe);
+    //$("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
     
     $("h1").bind("click", mouseClick);
     
+    
+    $('#replaceWText').bind('click', replaceWText);
+    $('#addPara').bind('click', addAPara);
+    $('#removePara').bind('click', removeAPara);
+    
 });
+
+function removeAPara(){
+    $('#randPara p:last').remove();
+}
+
+function addAPara(){
+    $('#addPara').append('<p>ADDED</p>');
+}
+
+function replaceWText(){
+    $('#replaceWText').text('Replaced!');
+}
 
 function mouseOverMe (){
     $("h1").html("armen is cool");
@@ -26,4 +43,8 @@ function mouseOverMe (){
 function mouseClick (){
     
     $("p").html("Clicked!!!!!!!!!!!!!!!!!!!!!!!!!")
+}
+
+function addAPara() {
+    $('#randPara').append('Armen is hot');
 }
